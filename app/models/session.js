4f351c990d744609c232d294dffefc0989f83f71
MKS-Student <mks.student@hackreactor.com>
2015-04-06 19:21:34 -0500

    final add

--- app/models/session.js
+++ app/models/session.js
@@ -11,3 +11,7 @@ var Session = db.Model.extend({
 });
 
 module.exports = Session;
+
+//%242a%2410%24BVbonrqUej2PlzLYfXiGjulerWpgfmHSFH0S6nl5umNt%2F0MsGjYaa
+//$2a$10$BVbonrqUej2PlzLYfXiGjulerWpgfmHSFH0S6nl5umNt/0MsGjYaa
+//$2a$10$BVbonrqUej2PlzLYfXiGjulerWpgfmHSFH0S6nl5umNt/0MsGjYaa
